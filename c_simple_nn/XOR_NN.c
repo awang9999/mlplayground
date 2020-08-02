aec603198717675e3930efd8435ac06571678cbf
Alexander Wang <aw576@cornell.edu>
2020-08-02 02:10:47 -0700

    made a simple makefile

--- c_simple_nn/XOR_NN.c
+++ c_simple_nn/XOR_NN.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include "maths.h"
 
 int main()
 {
+    int my_num = sum(5, 6);
+    printf("The special number is %d\n", my_num);
+    return -1;
 }
---
+++ c_simple_nn/maths.h
@@ -0,0 +1,9 @@
+#ifndef MATHS_H
+#define MATHS_H
+/* ^^ these are the include guards */
+
+/* Prototypes for the functions */
+/* Sums two ints */
+int sum(int a, int b);
+
+#endif
